1b87e4c93c21d8fc651b21967e916ae869b9dccd
Gyandeep <gyandeepmehra370@gmail.com>
2022-10-23 23:04:50 +0530

    Add total.js

---
+++ totalWatchTime.js
@@ -0,0 +1,39 @@
+let timestamp = [];
+function calculate(list) {
+    for (let i = 0; i < list.length; ++i) {
+        let obj = list[i];
+        let duration = obj.duration.split(/\r?\n/);
+
+        if (duration.length > 1) {
+            timestamp.push(duration[1].trim());
+        } else {
+            timestamp.push(duration[0]);
+        }
+    }
+
+    let totalHour = 0;
+    let totalMinutes = 0;
+    let totalSeconds = 0;
+    for (let i = 0; i < timestamp.length; ++i) {
+        let time = timestamp[i].split(':');
+        if (time.length == 3) {
+            totalHour += parseInt(time[0]);
+            totalMinutes += parseInt(time[1]);
+            totalSeconds += parseInt(time[2]);
+        } else if (time.length == 2) {
+            totalMinutes += parseInt(time[0]);
+            totalSeconds += parseInt(time[1]);
+        }
+    }
+
+    // console.log(totalHour + " - " + totalMinutes + " - " + totalSeconds);
+    let sec = totalSeconds % 60;
+    let min = (totalMinutes + parseInt((totalSeconds / 60 + "").split(".")[0])) % 60;
+    let hour = totalHour + parseInt((totalMinutes + parseInt((totalSeconds / 60 + "").split(".")[0])) / 60 + "");
+    let timeStr = hour + ":" + min + ":" + sec;
+    return timeStr;
+}
+
+module.exports = {
+    calculate: calculate
+};
